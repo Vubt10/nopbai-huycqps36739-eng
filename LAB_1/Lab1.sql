a95fdb54d15cefe6e2d990deb4248dc68830040c
huycqps36739-eng <huycqps36739@fpt.edu.vn>
2025-10-02 12:49:08 +0700

    huycqps36739_Lab1

---
+++ LAB_1/Lab1.sql
@@ -0,0 +1,12 @@
+﻿use QLDA;
+
+select * from NHANVIEN;
+select NGSINH, DCHI from NHANVIEN
+	where HONV = N'Đinh' and TENLOT = N'Bá' and TENNV = N'Tiên';
+--select CONCAT(HONV,' ',TENLOT,' ',TENNV) as Ten from NHANVIEN
+	--where HONV like N'N%' ;
+--select CONCAT(HONV,' ',TENLOT,' ',TENNV) as Ten from NHANVIEN
+  -- where DCHI like N'%HCM%' ;
+--select * from NHANVIEN where LUONG > 25000 and PHG = 4 or (PHG = 5 and LUONG > 30000)
+--select * from NHANVIEN where LUONG > 30000;
+--select * from NHANVIEN where PHG = 4;
